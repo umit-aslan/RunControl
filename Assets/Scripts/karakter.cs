2297f7efe298a3482ede3d676b09a0147efb736f
Ümit Aslan <prototprotot2@gmail.com>
2022-06-17 13:20:35 +0300

    karakter script added

---
+++ Assets/Scripts/karakter.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class karakter : MonoBehaviour
+{
+   private void FixedUpdate()
+   {
+        transform.Translate(Vector3.forward * .5f * Time.deltaTime);
+   }
+
+   private void Update() 
+   {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (Input.GetAxis("Mouse X") < 0)
+            {
+                transform.position =Vector3.Lerp(transform.position,new Vector3(transform.position.x-.1f,
+                 transform.position.y, transform.position.z), .3f);
+            }
+            else if (Input.GetAxis("Mouse X") > 0)
+            {
+                 transform.position =Vector3.Lerp(transform.position,new Vector3(transform.position.x+.1f,
+                 transform.position.y, transform.position.z), .3f);
+            }
+        }
+    }
+}
