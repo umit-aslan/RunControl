3e162855ca9e9a21f58dc522d54a2e4b970a3dd2
Ümit Aslan <79724156+umit-aslan@users.noreply.github.com>
2022-06-24 11:13:27 +0300

    Comment lines added

--- Assets/Scripts/Kamera.cs
+++ Assets/Scripts/Kamera.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class Kamera : MonoBehaviour
 {
-    public Transform target;
-    public Vector3 offset;
+    public Transform target;//Kameranın takip edeceği hedef(karakter).
+    public Vector3 offset;//Belli bir açıdan takip edebilmesi için offset yani bir pozisyon verdik.
 
     void Start()
     {
-        offset = transform.position - target.position;//Kamera ve  player arasındaki mesafeyi hesaplar
+        offset = transform.position - target.position;//Kameramızın pozisyonundan hedef pozisyonu çıkardık. Yani aradaki mesafeyi hesapladık.
     }
 
     void LateUpdate()
     {
         transform.position = Vector3.Lerp(transform.position, target.position + offset,.25f);
+        //Yumuşak bir takip için Vector3.Lerp kullandık.
+        //Bu hareketi hangi yumuşaklıkta yapacağını float olarak belirledik.
+        //Bu değer  genel olarak 100-150 arası olmalıdır.
     }
 }
